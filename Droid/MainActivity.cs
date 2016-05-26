9e3cc786d00bc061224f3ba01a4642bf1831ccaf
Fang Huai An <developer.fang@gmail.com>
2016-05-26 17:56:07 +0800

    add listview

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,13 +1,15 @@
-﻿using Android.App;
+﻿using System.Collections.Generic;
+using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Views;
 
 namespace XamarinTableView.Droid
 {
 	[Activity (Label = "XamarinTableView", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
+	    private ListView _myListView;
 
 		protected override void OnCreate (Bundle savedInstanceState)
 		{
@@ -16,14 +18,77 @@ namespace XamarinTableView.Droid
 			// Set our view from the "main" layout resource
 			SetContentView (Resource.Layout.Main);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button> (Resource.Id.myButton);
-			
-			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
-			};
+            // Get our button from the layout resource,
+            // and attach an event to it
+		    _myListView = FindViewById<ListView>(Resource.Id.myListView);
+
+		    LoadData();
+
 		}
+
+	    private void LoadData()
+	    {
+
+	        var list = new List<MyClass>
+	        {
+	            new MyClass {Name = @"Aa", Description = @"aaaaaaa"},
+	            new MyClass {Name = @"Bb", Description = @"cccbbbaaa", ImageUrl = @"https://"},
+	            new MyClass {Name = @"Cc", Description = @"cccaaaaaaa"},
+	            new MyClass {Name = @"Dd", Description = @"ddcccaaaaaaa", ImageUrl = @"https://"}
+	        };
+
+            _myListView.Adapter = new MyClassListAdapter( list, this );
+	        _myListView.ItemClick += (sender, args) =>
+	        {
+	            MyClass myclass = list[args.Position];
+
+	        };
+
+
+
+	    }
+
+        public class MyClassListAdapter : BaseAdapter<MyClass>
+        {
+            private Activity context;
+
+            private List<MyClass> MyClasses { get; set; }
+
+            public MyClassListAdapter(IEnumerable<MyClass> myclssesClasses, Activity context)
+            {
+                this.context = context;
+                MyClasses = new List<MyClass>();
+
+                MyClasses.AddRange(myclssesClasses);
+            }
+
+
+            public override long GetItemId(int position)
+            {
+                return position;
+            }
+
+            public override View GetView(int position, View convertView, ViewGroup parent)
+            {
+                MyClass myClass = MyClasses[position];
+
+                var view = convertView;
+
+                if (null == view)
+                {
+                    this.context.LayoutInflater.Inflate(Resource.Layout.myclass_listview_itemview, null);
+
+                }
+
+
+                return view;
+            }
+
+            public override int Count => MyClasses.Count;
+
+            public override MyClass this[int position] => MyClasses[position];
+        }
+
 	}
 }
 
